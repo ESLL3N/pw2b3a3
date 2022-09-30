ead5905a11abe17160b6d2d01c6d39b3caa849df
ESLL3N <esllenescolar@gmail.com>
2022-09-30 17:03:25 +0000

    avaliação

---
+++ pw2b3a3/src/main/java/etec/palmital/controllers/NumerosController.java
@@ -0,0 +1,27 @@
+package etec.palmital.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class NumerosController {
+    @RequestMapping("/")
+    public String calc(Model model) {
+        int[] resultado = null;
+        for(int val = 2 ; val < 30 ; val++) {
+            if(val % 2 == 0) {
+                if(resultado == null) {
+                    resultado = new int[1];
+                } else {
+                    int[] temp = resultado;
+                    resultado = new int[temp.length + 1];
+                    for(int pos = 0;pos < temp.length;pos++)
+                        resultado[pos] = temp[pos];
+                }
+                resultado[resultado.length - 1] = val;
+            }
+        }
+        model.addAttribute("valores", resultado);
+        return "/numeros/sequencia";
+    }
+}
